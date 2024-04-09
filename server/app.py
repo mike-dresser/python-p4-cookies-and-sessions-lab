36a4b6bfc4ed03a3ffee84703c656076ed75d141
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2024-04-09 15:02:16 -0400

    completed

--- server/app.py
+++ server/app.py
@@ -17,18 +17,25 @@ db.init_app(app)
 
 @app.route('/clear')
 def clear_session():
+    session['page_views'] = session.get('page_views')
+    print('Page views before', session['page_views'])
     session['page_views'] = 0
-    return {'message': '200: Successfully cleared session data.'}, 200
+    return make_response({'message': '200: Successfully cleared session data.'}, 200)
 
 @app.route('/articles')
 def index_articles():
-
-    pass
+    articles = [article.to_dict() for article in Article.query.all()]
+    return make_response(articles, 200)
 
 @app.route('/articles/<int:id>')
 def show_article(id):
-
-    pass
-
+    session['page_views'] = session.get('page_views') or 0
+    print(session['page_views'])
+    session['page_views'] += 1
+    if session['page_views'] > 3:
+        return make_response({'message': 'Maximum pageview limit reached'}, 401)
+    article =Article.query.filter_by(id=id).first().to_dict()
+    return make_response(article, 200)
+    
 if __name__ == '__main__':
     app.run(port=5555)
